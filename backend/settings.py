6b2a09b20344513d2c7bcbfa80a2d0699c7d89de
julianorossatovehio <64209448+julianorossatovehio@users.noreply.github.com>
2022-06-30 08:26:17 -0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'z8*^9cd*0g#18(=cy&rzfa9wj4oowb&*8r$_uw6+f7qeh=t9##'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'http://44.202.255.149:80/', 'http://44.202.255.149:8080/', 'http://127.0.0.1:8080','http://127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'http://44.202.255.149:80/', 'http://44.202.255.149/', 'http://44.202.255.149:8080/', 'http://127.0.0.1:8080', 'http://127.0.0.1:80','http://127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
